c9746f8a309f48c59e92a6d27da6006bb39fd242
Chanice Irungu <chaniceashley@gmail.com>
2021-09-09 03:59:01 +0300

    diabled-eslint

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Layout from "../components/Layout";
 import { Button, Grid, Hidden } from "@material-ui/core";
 import useStyles from "../utils/styles";
